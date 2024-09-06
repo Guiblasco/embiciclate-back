a06c0583d36c949db898daa0ab8306804f77377e
Guillem Blasco <gowet92@gmail.com>
2024-09-06 16:08:42 +0200

    Install cors and morgan

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,23 @@
 import express from "express";
+import morgan from "morgan";
+import cors, { type CorsOptions } from "cors";
 import getPing from "./healthCheck/getPing.js";
 import notFoundEndpoint from "./error/notFoundEndpoint/notFoundEndpoit.js";
 import { generalError } from "./error/generalError/generalError.js";
 
 const app = express();
+const origins: CorsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "https://guillem-blasco-202406-front.netlify.app",
+  ],
+};
 
 app.disable("x-powered-by");
+
+app.use(cors(origins));
+
+app.use(morgan("dev"));
 
 app.get("/", getPing);
 
