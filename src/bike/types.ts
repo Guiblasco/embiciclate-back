7e4736e3d061a9e65adb0ff33f2c3558404603bb
Guillem Blasco <gowet92@gmail.com>
2024-09-10 00:06:44 +0200

    Add bike types

---
+++ src/bike/types.ts
@@ -0,0 +1,10 @@
+export interface BikeStructure {
+  id: string;
+  brand: string;
+  model: string;
+  wheelsize: string;
+  material: string;
+  specs: string;
+  imageUrl: string;
+  alternativeText: string;
+}
