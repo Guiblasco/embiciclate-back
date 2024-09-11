0ba6e69436c44aff633273028e999fb25c4412f8
Guillem Blasco <gowet92@gmail.com>
2024-09-11 10:55:21 +0200

    Add bikes-router

---
+++ src/bike/router/bikesRouter.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import BikesController from "../controller/BikesController/BikesController.js";
+import Bike from "../model/Bike.js";
+
+const bikesRouter = express.Router();
+
+const bikesController = new BikesController(Bike);
+
+bikesRouter.get("/", bikesController.getBikes);
+
+export default bikesRouter;
