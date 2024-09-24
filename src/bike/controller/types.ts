70e86d31da7464e9835dd9af06ff12e06358abc1
Guillem Blasco <gowet92@gmail.com>
2024-09-24 22:39:58 +0200

    Add deleteBikeById method

--- src/bike/controller/types.ts
+++ src/bike/controller/types.ts
@@ -3,4 +3,7 @@ import type { NextFunction, Request, Response } from "express";
 export interface BikesControllerStructure {
   getBikes: (req: Request, res: Response, next: NextFunction) => void;
   addBikes: (req: Request, res: Response, next: NextFunction) => void;
+  deleteBikeById: (req: Request, res: Response, next: NextFunction) => void;
 }
+
+export type RequestWithBikeId = Request<{ bikeId: string }>;
